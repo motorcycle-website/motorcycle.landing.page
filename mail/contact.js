a875a8c1a9102a7a60dbaa3222dc54f449742f4d
anhpt <anhpt@basebs.com>
2023-08-23 11:54:08 +0700

    Update information

---
+++ mail/contact.js
@@ -0,0 +1,64 @@
+$(function () {
+
+    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault();
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var subject = $("input#subject").val();
+            var message = $("textarea#message").val();
+
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true);
+
+            // Using Email.js to send the email
+            var templateParams = {
+                from_email: email,
+                from_name: name,
+                to_email: "hcm.royalenfield@gmail",
+                to_name: 'Royal Enfield Việt Nam',
+                subject: subject,
+                message: message
+            };
+
+            emailjs.send("service_nbtnt8q", "template_jaiag9f", templateParams, '8bXymJ_5ka-ErGu1e')
+                .then(function(response) {
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                    $('#success > .alert-success')
+                            .append("<strong>Tin nhắn của bạn đã được gửi thành công! </strong>");
+                    $('#success > .alert-success')
+                            .append('</div>');
+                    $('#contactForm').trigger("reset");
+                }, function(error) {
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                    $('#success > .alert-danger').append($("<strong>").text("Xin lỗi " + name + ", đã xảy ra lỗi trong quá trình gửi tin nhắn. Vui lòng thử lại sau!"));
+                    $('#success > .alert-danger').append('</div>');
+                    $('#contactForm').trigger("reset");
+                })
+                .finally(function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false);
+                    }, 1000);
+                });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+$('#name').focus(function () {
+    $('#success').html('');
+});
